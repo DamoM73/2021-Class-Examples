9e15ccf81ab5b0ff43bbe3896303f496fc3f4be5
Damien Murtagh <MurtaghD@mbbc.qld.edu.au>
2020-04-24 10:48:37 +1000

    Prime numbers exercise 2a

---
+++ Unit 1/prime exercise 2a.py
@@ -0,0 +1,30 @@
+def is_prime(candidate):
+    # determines if candidate is a prime number
+    prime = True
+    dividend = 2
+    while prime:
+        if candidate % dividend == 0:
+            return False
+        elif candidate - dividend == 1:
+            return True
+        else:
+            dividend += 1
+
+def find_next_prime(start):
+    if start < 3:
+        next_num = 3
+    else:
+        next_num = start
+
+    while True:
+        if is_prime(next_num):
+            return(next_num)
+        else:
+            next_num += 1
+
+# ----- MAIN PROGRAM -----
+highest_prime = 0
+
+while True:
+    highest_prime = find_next_prime(highest_prime + 1)
+    print(highest_prime)
---
+++ Unit 1/scratch.py
@@ -0,0 +1,7 @@
+def spam():
+    print("spam")
+
+
+
+while True:
+    spam()
