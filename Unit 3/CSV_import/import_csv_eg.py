660aa6f7aa12ab5feb0d05d93b6f2f1d66a3953c
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2021-03-15 10:21:50 +1000

    start db

---
+++ Unit 3/CSV_import/import_csv_eg.py
@@ -0,0 +1,24 @@
+def create_table():
+    # create a table within the given database
+    pass
+
+
+def create_database():
+    # if database is not present, creates the required database
+
+    # create classes table
+    create_table()
+
+def import_csv():
+    # extreacts the data from provided CVS files and enters into the database
+    pass
+
+def query_db():
+    # runs the given query on the database
+    pass
+
+def export_csv():
+    # saves the proved data to csv file
+    pass
+
+# ===== MAIN PROGRAM =====
